0a2ef840f13f0f1f572aed117f942d7bfd4df341
John Stockinger <johnstockinger@gmail.com>
2021-04-20 22:53:18 -0600

    additional logging

--- server/server.js
+++ server/server.js
@@ -12,18 +12,23 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/login', (req, res) => {
     const code = req.body.code
-    const spotifyApi = new SpotifyWebApi({
-        redirectUri:  process.env.REACT_APP_REDIRECT_URL,
-        clientId: process.env.REACT_APP_CLIENT_ID,
-        clientSecret: process.env.REACT_APP_CLIENT_SECRET,
-    })
 
     console.log("code=")
     console.log(code)
     console.log(process.env.REACT_APP_CLIENT_ID)
     console.log(process.env.REACT_APP_REDIRECT_URL)
 
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri:  process.env.REACT_APP_REDIRECT_URL,
+        clientId: process.env.REACT_APP_CLIENT_ID,
+        clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+    })
+
+
+
     spotifyApi.authorizationCodeGrant(code).then(data => {
+        console.log("data")
+        console.log(data)
         res.json({
             accessToken: data.body.access_token,
             refreshToken: data.body.refresh_token,
